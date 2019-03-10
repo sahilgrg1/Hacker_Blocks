8bf7c13b512dd33faf4f676669141fc3784bf5f6
ACER-PC <gargsahil898@gmail.com>
2019-03-10 12:32:26 +0530

    updated

--- Increasion_Seq.java
+++ Increasion_Seq.java
@@ -15,22 +15,22 @@ public class Increasion_Seq {
     }
     public static boolean inctodec(int[] nums)
     {
+        boolean s = false;
         for (int i = 1; i < nums.length ; i++) {
-            if(nums[i] > nums[i-1])
+            if(nums[i] > nums[i-1] && s == false)
             {
-                return true;
+                s = true;
             }
-            if(nums[i] < nums[i-1]) {
-                return true;
+            if(nums[i] < nums[i-1] && s) {
+                return false;
             }
-            if(nums[i] == nums[i-1])
+            if(nums[i] == nums[i-1] )
             {
                 return false;
             }
-            if(nums[i] < nums[i-1] && nums[i] > nums[ i- 1])
-                return true;
+
 
         }
-        return false;
+        return true;
     }
 }
